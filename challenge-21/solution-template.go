7d520ab33e7269d7b198842e0892a17bcca12f92
nerolucas <lima.lucas.ma@gmail.com>
2025-12-07 21:05:47 -0300

    Challenge 21 completed

--- challenge-21/solution-template.go
+++ challenge-21/solution-template.go
@@ -27,19 +27,61 @@ func main() {
 // Returns the index of the target if found, or -1 if not found.
 func BinarySearch(arr []int, target int) int {
 	// TODO: Implement this function
+	left, right := 0, len(arr)-1
+	for left <= right {
+		current := left + (right-left)/2
+		if arr[current] == target {
+			return current
+		}
+		if arr[current] < target {
+			left = current + 1
+		} else { // arr[current] > target
+			right = current - 1
+		}
+	}
 	return -1
 }
 
 // BinarySearchRecursive performs binary search using recursion.
 // Returns the index of the target if found, or -1 if not found.
 func BinarySearchRecursive(arr []int, target int, left int, right int) int {
-	// TODO: Implement this function
-	return -1
+	if right < left {
+		return -1
+	}
+	current := left + (right-left)/2
+	if arr[current] < target {
+		return BinarySearchRecursive(arr, target, current+1, right)
+	} else if arr[current] > target {
+		return BinarySearchRecursive(arr, target, left, current-1)
+	} else {
+		return current
+	}
 }
 
 // FindInsertPosition returns the index where the target should be inserted
 // to maintain the sorted order of the array.
 func FindInsertPosition(arr []int, target int) int {
 	// TODO: Implement this function
-	return -1
+	if len(arr) == 0 {
+		return 0
+	}
+	left, right := 0, len(arr)-1
+	var current int
+	for left <= right {
+		current = left + (right-left)/2
+		if target <= arr[left] {
+			return left
+		} else if target > arr[right] {
+			return right + 1
+		} else {
+			if target > arr[current] {
+				left = current + 1
+			} else if target < arr[current] {
+				right = current - 1
+			} else {
+				break
+			}
+		}
+	}
+	return current
 }
